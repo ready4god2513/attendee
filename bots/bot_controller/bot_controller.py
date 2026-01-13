1561a24a1253c20a9595012990c9079411d7b261
Rafael Dantas Justo <rafael.justo@teamwork.com>
2026-01-13 12:36:51 -0300

    Fix: Redis connection with TLS support
    When connecting to Redis using TLS the `ssl_cert_reqs` parameter must be
    provided.
    
    > URL must have parameter ssl_cert_reqs and this must be set to CERT_REQUIRED,
    > CERT_OPTIONAL, or CERT_NONE
    
    The new `REDIS_SSL_REQUIREMENTS` environment variable allow setting this value.

--- bots/bot_controller/bot_controller.py
+++ bots/bot_controller/bot_controller.py
@@ -724,7 +724,14 @@ class BotController:
         if self.redis_client:
             self.redis_client.close()
 
-        redis_url = os.getenv("REDIS_URL") + ("?ssl_cert_reqs=none" if os.getenv("DISABLE_REDIS_SSL") else "")
+        redis_params = {}
+        if os.getenv("DISABLE_REDIS_SSL"): # backward compatibility
+            redis_params["ssl_cert_reqs"] = "none"
+        elif os.getenv("REDIS_SSL_REQUIREMENTS") is not None and os.getenv("REDIS_SSL_REQUIREMENTS") != "":
+            redis_params["ssl_cert_reqs"] = os.getenv("REDIS_SSL_REQUIREMENTS")
+        redis_params_query_string = "&".join([f"{key}={value}" for key, value in redis_params.items()])
+
+        redis_url = os.getenv("REDIS_URL") + ("?" + redis_params_query_string if redis_params_query_string else "")
         self.redis_client = redis.from_url(redis_url)
         self.pubsub = self.redis_client.pubsub()
         self.pubsub.subscribe(self.pubsub_channel)
