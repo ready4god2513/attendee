279e269a36d9e22a457f56e02c85cf8ec85953d3
Maciej Janowski <maciej.janowski@teamwork.com>
2026-01-22 11:52:15 +0100

    update soft limit

--- bots/tasks/run_bot_task.py
+++ bots/tasks/run_bot_task.py
@@ -10,7 +10,7 @@ from bots.bot_controller import BotController
 logger = logging.getLogger(__name__)
 
 
-@shared_task(bind=True, soft_time_limit=3600)
+@shared_task(bind=True, soft_time_limit=14400)  # 4 hours - must exceed BOT_MAX_UPTIME_SECONDS
 def run_bot(self, bot_id):
     logger.info(f"Running bot {bot_id}")
     bot_controller = BotController(bot_id)
